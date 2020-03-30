ff41e1b5933a1dd1bcda8d429b99421c7e71f588
Unknown <arseniyzhuck@mail.ru>
2020-03-30 16:01:15 +0300

    9378, Sum_matrix

---
+++ 9378.pas
@@ -0,0 +1,31 @@
+﻿var A: array[0..5] of integer;
+    i,n,x,pr,min,minch: integer;
+    
+    
+    begin
+      read(n);
+      for i:= 1 to 6 do read(a[i mod 6]);
+      
+      min:=1001;
+      minch:=1001;
+      pr:=1001*1001;
+      
+      for i:= 7 to n do
+      begin
+        
+          read(x);
+          if (x*a[i mod 6] mod 2 =0) and (x*a[i mod 6]<pr) then pr:=x*a[i mod 6];
+          if x mod 2 =0 then
+              begin 
+                  if x*min<pr then pr:= x*min;
+              end
+          else if x*minch<pr then pr:=x*minch;
+          if a[i mod 6]<min then min:=a[i mod 6];
+          if a[i mod 6] mod 2 = 0 then if a[i mod 6]<minch then minch:=a[i mod 6];
+          a[i mod 6]:=x;                 
+      end;
+      write(pr);
+      
+      
+      
+    end.
---
+++ summa_matrix.pas
@@ -0,0 +1,23 @@
+﻿var a: array[1..5,1..5] of integer;
+    i,j,n,m,s: integer;
+    
+begin
+  writeln('kol-vo strok: ');
+  read(n);
+  writeln('kol-vo stolbcov: ');
+  read(m);
+  
+  
+  for i:= 1 to n do
+      for j:= 1 to m do
+          read(a[i,j]);
+  
+  s:=0;
+  for i:= 1 to n do
+      for j:= 1 to m do
+          s:=s+a[i,j];
+          
+  write(S);
+  
+  
+end.
