fd396cd1c11a73761343ed23ad1f4d120e908f5b
Unknown <arseniyzhuck@mail.ru>
2020-03-31 18:06:07 +0300

    4 points

---
+++ 18575.pas
@@ -0,0 +1,36 @@
+﻿var b: array[0..5] of integer;
+    ost: array[0..2] of integer;
+    i,n,x,count,p: integer;
+    
+    
+begin
+  read(n);
+  for i:= 1 to 6 do read(b[i mod 6]);
+  
+  for i:= 0 to 2 do ost[i]:=0;
+  count:=0;
+  
+  for i:= 7 to n do 
+  begin
+      read(x);
+      p:=x mod 3;
+      if (x+b[i mod 6]) mod 3 =0 then inc(count);
+      count:=count+ost[(3-p) mod 3];
+      p:=b[i mod 6] mod 3;
+      inc(ost[p]);
+      b[i mod 6]:=x;
+      
+      {
+      if p=1 then count:=count+ost[2];
+      if p=2 then count:=count+ost[1];
+      if p=0 then count:=count+ost[0];
+      p:=b[i mod 6] mod 3;
+      if p=0 then inc(ost[0]);
+      if p=1 then inc(ost[1]);
+      if p=2 then inc(ost[2]);     
+      }
+    end;
+    write(count);
+  
+  
+end.
---
+++ 9777.pas
@@ -0,0 +1,13 @@
+﻿var i,n,x,y,xmax,ymax: integer;
+begin
+  read(n);
+  xmax:=0; ymax:=0;
+  for i:= 1 to n do
+  begin
+    read(x);
+    read(y);
+    if x=0 then if abs(y)>ymax then ymax:=abs(y);
+    if y=0 then if abs(x)>xmax then xmax:=abs(x);
+  end;
+  write(xmax*ymax/2);
+end.
