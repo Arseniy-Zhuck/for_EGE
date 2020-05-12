728c551cdd5e346d1d0dff9e659bca0ad0115dd0
Unknown <arseniyzhuck@mail.ru>
2020-05-12 16:14:28 +0300

    Katy_12/05

---
+++ 18729_4_points.pas
@@ -0,0 +1,32 @@
+﻿var mass: array[0..117] of integer;
+    ost,i,n,a,left,right,sum: integer;
+begin
+  read(n);// [2,3,116,115,...] ->  mass[0,0,2,3,...,85,...,116]  a -> i 
+  for i:=0 to 117 do mass[i]:=0;
+  left:=0; right:=0; sum:=0;
+  for i:= 1 to n do
+  begin
+      read(a);
+      ost:=a mod 117;
+      if ost>0 then
+          begin
+              if (a+mass[117-ost]>sum)and (mass[117-ost]>a) then
+                begin
+                  sum:=a+mass[117-ost];
+                  left:=mass[117-ost];
+                  right:=a;
+                end;
+          end
+      else
+          begin
+              if (a+mass[0]>sum) and (mass[0]>a) then
+                begin
+                  sum:=a+mass[0];
+                  left:=mass[0];
+                  right:=a;
+                end;
+          end;
+     if a>mass[ost] then mass[ost]:=a;
+  end;
+  writeln(left,'  ',right);
+end.
---
+++ 4862_4_points.pas
@@ -0,0 +1,34 @@
+﻿var
+i,x,y,n,xmaxp,xmaxn,ymaxp,yminp,ymaxn,yminn:integer;
+s:real;
+begin
+  xmaxp:=0;xmaxn:=0;ymaxp:=0;yminp:=maxint;ymaxn:=0;yminn:=maxint;s:=0;
+  readln(n);
+  for i:=1 to n do
+  begin
+      readln(x,y);
+      if y>0  then
+      begin
+          if x=0 then
+            begin
+              if y>ymaxp then ymaxp:=y;
+              if y<yminp then yminp:=y;
+            end
+          else if abs(x)>xmaxp then xmaxp:=abs(x);
+      end;
+      if y<0 then
+      begin
+          if x=0 then
+            begin
+              if abs(y)>ymaxn then ymaxn:=abs(y);
+              if abs(y)<yminn then yminn:=abs(y);
+            end
+          else if abs(x)>xmaxn then xmaxn:=abs(x);
+      end;
+  end;
+     
+     
+     s:=0.5*(ymaxp-yminp)*xmaxp;
+      if s>(0.5*(ymaxn-yminn)*xmaxn) then writeln(s)
+      else writeln (0.5*(ymaxn-yminn)*xmaxn);
+      end.
