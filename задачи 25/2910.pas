31c3863361cb60b412f598b91c5934f1f3edaec5
Unknown <arseniyzhuck@mail.ru>
2020-05-12 16:51:46 +0300

    2910

---
+++ задачи 25/2910.pas
@@ -0,0 +1,21 @@
+﻿var A: array[1..5,1..5] of integer;
+    i,j,n,max,sum: integer;
+    
+begin
+  read(n);
+  for i:= 1 to n do
+    for j:= 1 to n do
+      readln(A[i,j]);
+      
+  sum:=0;
+  for i:= 1 to n do
+  begin
+    max:=a[i,1];
+    for j:= 1 to n do 
+      if a[i,j]>max then max:=a[i,j];
+    sum:=sum+max;
+  end;
+  
+  writeln(sum);
+  
+end.
