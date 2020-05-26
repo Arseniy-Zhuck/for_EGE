358339d972a1934b5650fa359e4e0026ec11501e
Unknown <arseniyzhuck@mail.ru>
2020-05-26 20:37:30 +0300

    Polyakov

---
+++ Polyakov_555.pas
@@ -0,0 +1,11 @@
+﻿var i,n,x,y,max1,max2: integer;
+begin
+  read(n);
+  for i:= 1 to n do
+  begin
+    read(x,y);
+    if x=y then if abs(x)>max1 then max1:=abs(x);
+    if x=-y then if abs(x)>max2 then max2:=abs(x); 
+  end;
+  write(max1*max2);
+end.
---
+++ Polyakov_557.pas
@@ -0,0 +1,14 @@
+﻿const k=3;
+var i,n,a,sum,p: integer;
+    ost: array[0..k-1] of integer;
+    
+begin
+  read(n);
+  for i:= 1 to n do
+  begin
+    read(a);
+    p:=a mod 3;
+    if a+ost[(k-p)mod 3]>sum then sum:=a+ost[(k-p)mod 3];
+    if a>ost[p] then ost[p]:=a;
+  end;
+end.
